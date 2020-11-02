98ce993997db15e389aeab5721f2a7d61286d8f8
Shirley <shirley.apellido@gmail.com>
2020-11-02 11:09:55 +0800

    adding Navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './Nav.css';
 
 function Nav() {
     return (
-        <div>
-            
+        <div className="nav">
+            <img 
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png" 
+                alt="Netflix Logo" 
+                className="nav_logo"/>
         </div>
     )
 }
